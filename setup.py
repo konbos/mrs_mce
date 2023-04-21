8da01f39f920777e720940fdb1b6c0803bbdc881
Konstantin E. Bosbach <konstantin.bosbach@mars.uni-freiburg.de>
2023-04-21 11:19:45 +0200

    correct. version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fsl_mrs_mce',
-      version='0-0.01',
+      version='0.0.01',
       description='A fsl_mrs Moncte Carlo estimation approach',
       url='https://github.com/konbos/mrs-mce.git',
       author='Konstantin E Bosbach',
