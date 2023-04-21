ce44f8a8008a12e153ec13e1cb2aba6d1234fd68
konbos <129419362+konbos@users.noreply.github.com>
2023-04-21 12:38:08 +0200

    changed all names to 'mrs-mce'

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='fsl_mrs_mce',
+setup(name='mrs-mce',
       version='0.0.02',
       description='A fsl_mrs Monte Carlo estimation approach',
       url='https://github.com/konbos/mrs-mce.git',
       author='Konstantin E Bosbach',
       author_email='konstantin.bosbach@mars.uni-freiburg.de',
-      packages=['fsl_mrs_mce'],
+      packages=['mrs-mce'],
       zip_safe=False)
 
