5dca981dc97a09ec91830870fea6c090e27c0243
konbos <129419362+konbos@users.noreply.github.com>
2023-04-21 12:28:20 +0200

    typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fsl_mrs_mce',
       version='0.0.01',
-      description='A fsl_mrs Moncte Carlo estimation approach',
+      description='A fsl_mrs Monte Carlo estimation approach',
       url='https://github.com/konbos/mrs-mce.git',
       author='Konstantin E Bosbach',
       author_email='konstantin.bosbach@mars.uni-freiburg.de',
