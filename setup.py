451da0f9b7bac13d1cba5a62287dded9f031ea6b
konbos <129419362+konbos@users.noreply.github.com>
2023-05-04 13:43:21 +0200

    v++

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mrs_mce',
-      version='0.0.07',
+      version='0.0.08',
       description='A fsl_mrs Monte Carlo estimation approach',
       url='https://github.com/konbos/mrs_mce.git',
       author='Konstantin E Bosbach',
